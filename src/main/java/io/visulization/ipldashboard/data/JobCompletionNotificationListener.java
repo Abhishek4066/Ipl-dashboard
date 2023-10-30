1f7dc4d13324677cb2ee38f423b28421ccd8ed07
Abhishek Gawade <abhishekgawade227@gmail.com>
2023-10-30 10:32:03 +0530

    add logic to calculate no. of matches played by each team AND no. of matches won by each team

--- src/main/java/io/visulization/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/visulization/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,7 +1,10 @@
 package io.visulization.ipldashboard.data;
 
+import java.util.HashMap;
+import java.util.Map;
 
-
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,32 +12,64 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.DataClassRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import io.visulization.ipldashboard.model.Team;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
-  private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+    // Create a logger for this class to log messages
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-  private final JdbcTemplate jdbcTemplate;
+    // An EntityManager is used to interact with the JPA (Java Persistence API)
+    private final EntityManager em;
 
-  @Autowired
-  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
-  }
-
-  @Override
-  public void afterJob(JobExecution jobExecution) {
-    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-      log.info("!!! JOB FINISHED! Time to verify the results");
-
-      jdbcTemplate
-          .query("SELECT team1,team2, date FROM match", 
-          (rs,row) -> "Team 1" + rs.getString(1) + "Team 2" + rs.getString(2) + "Date " + rs.getString(3))  
-          .forEach(str -> System.out.println(str));
+    @Autowired
+    public JobCompletionNotificationListener(EntityManager em) {
+        this.em = em;
     }
-  }
 
+    @Override
+    @Transactional
+    public void afterJob(JobExecution jobExecution) {
+        // Check if the batch job was completed successfully
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+            // Create a map to store team data
+            Map<String, Team> teamData = new HashMap<>();
+
+            // Query the database to get the count of matches per team (team1)
+            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                .getResultList()
+                .stream()
+                .map(e -> new Team((String) e[0], (long) e[1]))
+                .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            // Query the database to get the count of matches per team (team2)
+            em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+                .getResultList()
+                .stream()
+                .forEach(e -> {
+                    Team team = teamData.get((String) e[0]);
+                    team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+                });
+
+            // Query the database to get the count of matches won per team
+            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+                .getResultList()
+                .stream()
+                .forEach(e -> {
+                    Team team = teamData.get((String) e[0]);
+                    if (team != null) team.setTotalWins((long) e[1]);
+                });
+
+            // Persist the team data in the database
+            teamData.values().forEach(team -> em.persist(team));
+
+            // Log the team data
+            teamData.values().forEach(team -> System.out.println(team));
+        }
+    }
 }
--- src/main/java/io/visulization/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/io/visulization/ipldashboard/data/MatchDataProcessor.java
@@ -10,49 +10,52 @@ import org.springframework.batch.item.ItemProcessor;
 
 import io.visulization.ipldashboard.model.Match;
 
-
-
 public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
 
-    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+	// Create a logger for this class to log messages
+	private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
 
-    @Override
-    public Match process(final MatchInput matchInput) throws Exception {
-        
-        Match match = new Match();
-        match.setId(Long.parseLong(matchInput.getId()));
-        match.setCity(matchInput.getCity());
+	@Override
+	public Match process(final MatchInput matchInput) throws Exception {
 
-        match.setDate(LocalDate.parse(matchInput.getDate()));
+		// Create a new Match object to transform and store the input data
+		Match match = new Match();
+		match.setId(Long.parseLong(matchInput.getId()));
+		match.setCity(matchInput.getCity());
 
-        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
-        match.setVenue(matchInput.getVenue());
+		// Parse and set the match date
+		match.setDate(LocalDate.parse(matchInput.getDate()));
 
-        // Set Team 1 and Team 2 depending on the innings order
-        String firstInningsTeam, secondInningsTeam;
+		match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+		match.setVenue(matchInput.getVenue());
 
-        if ("bat".equals(matchInput.getToss_decision())) {
-            firstInningsTeam = matchInput.getToss_winner();
-            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
-                ? matchInput.getTeam2() : matchInput.getTeam1();
+		// Set Team 1 and Team 2 depending on the toss decision
+		String firstInningsTeam, secondInningsTeam;
 
-        } else {
-            secondInningsTeam = matchInput.getToss_winner();
-            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
-                ? matchInput.getTeam2() : matchInput.getTeam1();
-        }
-        match.setTeam1(firstInningsTeam);
-        match.setTeam2(secondInningsTeam);
+		if ("bat".equals(matchInput.getToss_decision())) {
+			firstInningsTeam = matchInput.getToss_winner();
+			secondInningsTeam = matchInput.getToss_winner()
+					.equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+					: matchInput.getTeam1();
+		} else {
+			secondInningsTeam = matchInput.getToss_winner();
+			firstInningsTeam = matchInput.getToss_winner()
+					.equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+					: matchInput.getTeam1();
+		}
 
-        match.setTossWinner(matchInput.getToss_winner());
-        match.setTossDecision(matchInput.getToss_decision());
-        match.setMatchWinner(matchInput.getWinner());
-        match.setResult(matchInput.getResult());
-        match.setResultMargin(matchInput.getResult_margin());
-        match.setUmpire1(matchInput.getUmpire1());
-        match.setUmpire2(matchInput.getUmpire2());
+		// Set the teams and other match details
+		match.setTeam1(firstInningsTeam);
+		match.setTeam2(secondInningsTeam);
+		match.setTossWinner(matchInput.getToss_winner());
+		match.setTossDecision(matchInput.getToss_decision());
+		match.setMatchWinner(matchInput.getWinner());
+		match.setResult(matchInput.getResult());
+		match.setResultMargin(matchInput.getResult_margin());
+		match.setUmpire1(matchInput.getUmpire1());
+		match.setUmpire2(matchInput.getUmpire2());
 
-        return match;
-    }
-
+		// Return the processed Match object
+		return match;
+	}
 }
