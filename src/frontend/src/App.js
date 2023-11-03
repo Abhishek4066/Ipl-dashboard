5f4bfa96376ec6dd2fff9738e3be1cbc929d70e8
Abhishek Gawade <abhishekgawade227@gmail.com>
2023-11-03 19:58:21 +0530

    completed ui pages for match pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route
 
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        
         <Routes> {/* Wrap your routes within a <Routes> element */}
           <Route path="/teams/:teamName" element={<TeamPage />} /> {/* Use the "element" prop to define the component */}
+
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
         </Routes>
+        
       </Router>
     </div>
   );
